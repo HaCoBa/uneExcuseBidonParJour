5688afa67a3d1ccce4c5f06e9d720ab3d21675d2
HaCoBa <llamande.pro@gmail.com>
2021-01-19 19:30:05 +0100

    local connexion done - subject retrieve done

--- index.js
+++ index.js
@@ -6,25 +6,48 @@ const cron = require('node-cron');
 const dotenv = require('dotenv');
 // I call the config saved in my .env file
 dotenv.config();
+// I call postgre library
+const { Client } = require('pg');
 // I call the twitter library installed
 var Twitter = require('twitter');
 
 // Creating an instance Express
 app = express();
 
-// TODO : CONNECT TO DATABASE
+// Connecting to database with env information
+const clientDB = new Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: 5432,
+});
+clientDB.connect();
+// console.log(clientDB);
+
+let subject='';
+let verb='';
+let complement='';
+
+clientDB.query('SELECT * FROM "subject" WHERE id=1', (err, res)=> {
+    if(err) {
+        console.log(err);
+    } else {
+        subject = res.rows[0].name;
+        console.log(subject);
+    }
+});
+
+// TODO : CODE A CRON TASK
+// cron.schedule('10 * * * * *', function() {
+//     console.log('exécuter une tâche à chaque minute');
+// })
 
 // TODO : CODE CADAVRE EXQUIS PROGRAM
 
 // TODO : SET THE NEW TWEET WITH CADAVRE EXQUIS SENTENCE
 // I set a variable where I will stocked the content of each new tweet
 const new_tweet = '';
-
-// TODO : CODE A CRON TASK
-cron.schedule('* * * * *', function() {
-    console.log('exécuter une tâche à chaque minute');
-})
-
 
 // // To create that will post, I need to authenticate via
 // // the twitter library by using connexion informations
