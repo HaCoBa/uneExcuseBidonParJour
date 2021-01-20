ae0756c4802fb1acb01c79901914117b0365bf05
HaCoBa <llamande.pro@gmail.com>
2021-01-20 19:04:17 +0100

    Epic fail on the tweet content generation, pb with the query that won't return the result even though the result is good

--- index.js
+++ index.js
@@ -25,95 +25,10 @@ const clientDB = new Client({
 // Connecting to the database
 clientDB.connect();
 
-// TODO : CODE A CRON TASK
 // cron.schedule('10 * * * * *', function() {
 //     console.log('exécuter une tâche à chaque minute');
 
-    // Get a random if between an interval
-    function getRandomInDBTable(min, max) {
-        return Math.floor(Math.random()*(max-min)+min);
-    }
-
-    // Get a random string from a specified table
-    function getRandomString(table) {
-
-        clientDB.query(`SELECT COUNT (*) from ${table}`, (err, res) => {
-
-            if(err) {
-
-                console.log(err);
-
-            } else {
-
-                result = parseInt(res.rows[0].count);
-                // console.log('table length', result);
-                // console.log(typeof result);
-                let randomStringId = getRandomInDBTable(1, result);
-                // console.log('random string id', randomStringId);
-
-                clientDB.query(`SELECT * from ${table} WHERE id=${randomStringId}`, (err, res) => {
-
-                    if(err) {
-
-                        console.log(err);
-
-                    } else {
-
-                        result = res.rows[0].name;
-                        // console.log(result);
-                        return result; // Ici j'ai bien le terme que je veux
-                    }
-                })
-            }
-        });
-    }
-
-    let subject = getRandomString('subject'); // mais ici, c'est toujours undefined
-    console.log(subject);
-
-    // let subjectId = getRandomInDBTable(1, getTableLength());
-    // console.log(subjectId);
-
-    // let subject='';
-    // let verb='';
-    // let complement='';
-    // let new_tweet = '';
-
-    // clientDB.query('SELECT * FROM "subject" WHERE id=1', (err, res)=> {
-    //     if(err) {
-    //         console.log(err);
-    //     } else {
-    //         subject = res.rows[0].name;
-    //         console.log(subject);
-    //         new_tweet += subject + ' ';
-    //         console.log('new tweet =', new_tweet);
-
-    //     }
-    // });
-
-    // clientDB.query('SELECT * FROM "verb" WHERE id=1', (err, res)=> {
-    //     if(err) {
-    //         console.log(err);
-    //     } else {
-    //         verb = res.rows[0].name;
-    //         console.log(subject);
-    //         new_tweet += verb + ' ';
-    //         console.log('new tweet =', new_tweet);
-
-    //     }
-    // });
-
-    // clientDB.query('SELECT * FROM "complement" WHERE id=1', (err, res)=> {
-    //     if(err) {
-    //         console.log(err);
-    //     } else {
-    //         complement = res.rows[0].name;
-    //         console.log(subject);
-    //         new_tweet += complement + '... #ExcuseBidon';
-    //         console.log('new tweet =', new_tweet);
-
-    //     }
-    // });
+    
 
     // // To create that will post, I need to authenticate via
     // // the twitter library by using connexion informations
@@ -132,9 +47,6 @@ clientDB.connect();
     //     // console.log(response);  // Raw response object.
     // });
 
-
 // })
-
-
 
 app.listen(3000);
