1fc1bdf6d29801d4e3b6ed61836a9994352fef52
HaCoBa <llamande.pro@gmail.com>
2021-01-21 20:08:10 +0100

    Cleaning some code - basic function is ok, need the set the unique verification and archive update

--- index.js
+++ index.js
@@ -26,35 +26,94 @@ const clientDB = new Client({
 clientDB.connect(function(err) {
     if(err) {
         throw err;
-    } else {
-        console.log('Connected to databe excusebidon!');
     }
+    // else {
+    //     console.log('Connected to databe excusebidon!');
+    // }
 });
 
-// cron.schedule('10 * * * * *', function() {
-//     console.log('exécuter une tâche à chaque minute');
+// Scheduled task to generate 1 tweet per day
+// cron.schedule('* * * * *', function() {
+//     console.log('exécuter une tâche toutes les 5 secondes');
 
-    const getRandomId = function(min, max) {
-        return Math.floor(Math.random() * (max-min)+min);
+    // I set up an init function that  I parameter as async
+    // This way, I can use await properties for various function that need a response from a databese query
+    const init = async () => {
+
+    
+        const getRandomId = (min, max) => {
+            return Math.floor(Math.random() * (max-min)+min);
+        };
+    
+        const getRandomTerm = async (table, min, max) => {
+    
+            const randomId = getRandomId(min, max);
+      
+            const queryResponse = await clientDB.query(`SELECT * FROM ${table}`);
+            
+            return queryResponse.rows[randomId].name;
+    
+            // console.log("response", response);
+        };
+    
+        let new_tweet = '';
+
+        const buildNewTweet = async () => {
+            let subject = await getRandomTerm('subject', 1, 112);
+            let verb = await getRandomTerm('verb', 1, 112);
+            let complement = await getRandomTerm('complement', 1, 112);
+            return subject + ' ' + verb + ' ' + complement + '... #ExcuseBidon #CadavreExquis';
+        }
+    
+        new_tweet = await buildNewTweet();
+        console.log(new_tweet);
+      
+        let queryResponse = await clientDB.query(`SELECT * FROM archive`);
+        let archives = queryResponse.rows;
+        // console.log(archives);
+        
+        let databaseUpdate = await clientDB.query(`INSERT INTO archive VALUES ('test')`);
+
+        queryResponse = await clientDB.query(`SELECT * FROM archive`);
+        archives = queryResponse.rows;
+        console.log(archives);
+
+        archives.map(entrie => {
+            if(entrie.phrase !== new_tweet) {
+                console.log('le texte est différent');
+            } else {
+                console.log('le texte est identique');
+            }
+        })
+
+ 
+
+
+
+
+
+    
+        // To create that will post, I need to authenticate via
+        // the twitter library by using connexion informations
+        // set and found in the developer tools dashboard
+        const client = new Twitter({
+            consumer_key: process.env.API_KEY,
+            consumer_secret: process.env.API_SECRET_KEY,
+            access_token_key: process.env.ACCESS_TOKEN_KEY,
+            access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+        });
+    
+        // // I post the tweet
+        // client.post('statuses/update', {status: new_tweet},  function(error, tweet, response) {
+        //     console.log(error);  // Error body.
+        //     // console.log(tweet);  // Tweet body.
+        //     // console.log(response);  // Raw response object.
+        // });
+    
     }
+    
+    init();
 
-    // // To create that will post, I need to authenticate via
-    // // the twitter library by using connexion informations
-    // // set and found in the developer tools dashboard
-    // const client = new Twitter({
-    //     consumer_key: process.env.API_KEY,
-    //     consumer_secret: process.env.API_SECRET_KEY,
-    //     access_token_key: process.env.ACCESS_TOKEN_KEY,
-    //     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-    // });
-
-    // // I post the tweet
-    // client.post('statuses/update', {status: new_tweet},  function(error, tweet, response) {
-    //     console.log(error);  // Error body.
-    //     console.log(tweet);  // Tweet body.
-    //     // console.log(response);  // Raw response object.
-    // });
-
-// })
+// });
 
 app.listen(3000);
